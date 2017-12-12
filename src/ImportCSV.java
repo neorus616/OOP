fd18a57ed8dd41c7c812b8d1334bda11f7d899dc
neorus616 <33427780+neorus616@users.noreply.github.com>
2017-12-12 11:03:50 +0200

    Main upload
    Added description to all classes, junit to most classes. Renamed all classes to meaningful names.

---
+++ src/ImportCSV.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+/**
+ * 
+ * @author Kostia Kazakov &amp; Yogev Rahamim <br>  
+ * @version 2.0
+ * <b>Description:</b> <br>
+ * Receive path to CSV file\s directory from user and validate the path. <br>
+ * Use Commons Apache library to validate and import the CSV file in the desirable format. <br>
+ * Create object "networks" which stores the records of the content.
+ * then, send it to ExportCSV class for further processing.
+ */
+public class ImportCSV {
+
+	/**
+	 * @param path - User input for the CSV directory.
+	 * @exception IllegalArgumentException - No CSV files or not a directory.
+	 */
+	public static void validPath(String path) {
+		int counter = 0;
+		File f = new File(path);
+		if(f.isDirectory()) {
+			File[] a = f.listFiles();
+			for(int i = 0; i < a.length; i++) {
+				String fileName = a[i].getName();
+				String extension = "";
+				int k = fileName.lastIndexOf('.');
+				if (k > 0)
+					extension = fileName.substring(k+1);
+				if(extension.compareTo("csv") == 0) {
+					ImportCSV.readCSV(path, fileName);
+					counter++;
+				}
+			}
+			if(counter == 0) {
+				throw new IllegalArgumentException("there is no CSV files");
+			}
+		}
+		else {
+			throw new IllegalArgumentException(path + " is Not a Directory");
+		}
+
+	}
+
+	/**
+	 * @param path - Directory path.
+	 * @param fileName - File name.
+	 * @exception IllegalArgumentException - The file corrupted or in wrong format.
+	 */
+	static void readCSV(String path, String fileName) {
+		try {
+			FileReader in = new FileReader(path + fileName);
+			BufferedReader bufferedReader = new BufferedReader(in);
+			String csvSplitBy = ",";
+			String[] catchModel = bufferedReader.readLine().split(csvSplitBy);	
+			Hashtable<String, Networks> strongPoints =  new Hashtable<>();
+			if(catchModel.length > 2 && catchModel[2].contains("model")) {
+				String model = catchModel[2].substring(6, catchModel[2].length());
+				Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(bufferedReader);
+				Networks network = new Networks();
+				for (CSVRecord record : records) {
+					String mac = record.get("MAC");
+					String ssid = record.get("SSID");
+					String time = record.get("FirstSeen");
+					int channel = Integer.parseInt(record.get("Channel"));
+					int signal = Integer.parseInt(record.get("RSSI"));
+					double lat = Double.parseDouble(record.get("CurrentLatitude"));
+					double lon = Double.parseDouble(record.get("CurrentLongitude"));
+					double alt = Double.parseDouble(record.get("AltitudeMeters"));
+					if(strongPoints.containsKey(time))
+						strongPoints.get(time).add(ssid, mac, signal, channel);
+					else {
+						network = new Networks(model, time, lat, lon, alt);
+						network.add(ssid, mac, signal, channel);
+						strongPoints.put(time, network);
+					}
+				}
+				ExportCSV.writeCsvFile(strongPoints, path + "newUpgradedCSV.csv");
+			}
+			else System.out.println(fileName + " is not a valid CSV format !");
+
+		} catch (IOException | IllegalArgumentException e) {
+			throw new IllegalArgumentException(fileName + " is not a valid CSV file !");
+
+		}
+	}
+}
---
+++ src/NetworksTest.java
@@ -0,0 +1,66 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class NetworksTest {
+
+	Networks networkTest = new Networks("ID", "Time", 0, 0, 0);
+	ArrayList<Wifi> xpoints = new ArrayList<Wifi>();
+	
+	
+	@Test
+	final void testAdd() {
+		networkTest.add("ssid1","mac", -80, 10);
+		networkTest.add("ssid2","mac", -65, 10);
+		networkTest.add("ssid3","mac", -99, 10);
+		networkTest.add("ssid4","mac", -120, 10);  //worse
+		networkTest.add("ssid5","mac", -98, 10);
+		networkTest.add("ssid6","mac", -99, 10);
+		networkTest.add("ssid7","mac", -87, 10);
+		networkTest.add("ssid8","mac", -75, 10);
+		networkTest.add("ssid9","mac", -88, 10);
+		networkTest.add("ssid10","mac", -93, 10);
+		networkTest.add("ssid11","mac", -83, 10);
+		assertEquals(networkTest.getPoints().get(3).getSignal(),-83 );
+	}
+
+	@Test
+	final void testGetID() {
+		assertEquals(networkTest.getID(),"ID");
+	}
+
+	@Test
+	final void testGetTime() {
+		assertEquals(networkTest.getTime(),"Time");
+	}
+
+	@Test
+	final void testGetPoints() {
+		Wifi p1 = new Wifi("ssid1","mac", -80, 10);
+		xpoints.add(p1);
+		Wifi p2 = new Wifi("ssid2","mac", -65, 10);
+		xpoints.add(p2);
+		Wifi p3 = new Wifi("ssid3","mac", -99, 10);
+		xpoints.add(p3);
+		Wifi p4 = new Wifi("ssid11","mac", -83, 10);
+		xpoints.add(p4);
+		Wifi p5 = new Wifi("ssid5","mac", -98, 10);
+		xpoints.add(p5);
+		Wifi p6 = new Wifi("ssid6","mac", -99, 10);
+		xpoints.add(p6);
+		Wifi p7 = new Wifi("ssid7","mac", -87, 10);
+		xpoints.add(p7);
+		Wifi p8 = new Wifi("ssid8","mac", -75, 10);
+		xpoints.add(p8);
+		Wifi p9 = new Wifi("ssid9","mac", -88, 10);
+		xpoints.add(p9);
+		Wifi p10 = new Wifi("ssid10","mac", -93, 10);
+		xpoints.add(p10);
+		assertEquals(networkTest.getPoints(),xpoints);
+	}
+
+
+}
